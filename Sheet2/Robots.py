05f602a916f221c0e733765d979ff607025605b7
Rami Hawi <ramihawi7@gmail.com>
2023-07-17 09:57:28 -0700

    h

--- Sheet2/Robots.py
+++ Sheet2/Robots.py
@@ -1,19 +1,20 @@
 from gasp import *          
-
+def lines():
+    for y in range(0,480,10):
+        Line((0,y),(640,y),thickness=.01,color='lightgray')
+    for x in range(0,640,10):
+        Line((x,0),(x,640),thickness=.01,color='lightgray')
 begin_graphics()            
-for y in range(0,480,10):
-    Line((0,y),(640,y),thickness=.01,color='lightgray')
-for x in range(0,640,10):
-    Line((x,0),(x,640),thickness=.01,color='lightgray')
+lines()
 
 
 finished = False
-def place_player():
-    print("Here I am!")
+def place_player(player_x,player_y):
+    Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
 
 def move_player():
-    print("I'm moving...")
-place_player()
+    #hello
+place_player(30,30)
 
 while not finished:
     move_player()
