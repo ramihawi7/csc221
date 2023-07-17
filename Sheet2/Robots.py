8f4e25744281a3310b6a9e4e03dab7c4bc1eb60d
Rami Hawi <ramihawi7@gmail.com>
2023-07-17 11:12:42 -0700

    Moving

--- Sheet2/Robots.py
+++ Sheet2/Robots.py
@@ -1,23 +1,35 @@
-from gasp import *          
+from gasp import *
 def lines():
+    line_x = 0
+    line_y = 0
     for y in range(0,480,10):
         Line((0,y),(640,y),thickness=.01,color='lightgray')
     for x in range(0,640,10):
         Line((x,0),(x,640),thickness=.01,color='lightgray')
+
+def place_player(player_x,player_y):
+    Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
 begin_graphics()            
 lines()
 
+player_x= 30
+player_y= 30 
 
-finished = False
-def place_player(player_x,player_y):
-    Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
 
-def move_player():
-    #hello
-place_player(30,30)
-
-while not finished:
-    move_player()
+while True: 
+    player = Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
+    key = update_when('key_pressed')
+    remove_from_screen(player)
+    if key == 'Right':
+        player_x += 1
+    if key == 'Left':
+        player_x -= 1
+    if key == 'Up':
+        player_y += 1
+    if key == 'Down': 
+        player_y -= 1
+    if key == 'q':
+        break
 
 update_when('key_pressed')
 end_graphics() 
