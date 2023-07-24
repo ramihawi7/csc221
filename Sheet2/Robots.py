fc048c8e0a33fd5647423ece28043426f2bfc6be
Rami Hawi <ramihawi7@gmail.com>
2023-07-24 09:57:54 -0700

    Teloportation added

--- Sheet2/Robots.py
+++ Sheet2/Robots.py
@@ -1,4 +1,89 @@
 from gasp import *
+import time
+from random import randint
+def loose():
+    time.sleep(1)
+    clear_screen()
+    Text("You Lost :'c",(320, 270), size=40)
+    key_text = Text("Press esc to stop playing", (320, 100), size=12)
+    en = update_when('key_pressed')
+    clear_screen()
+    if en == "Escape":
+        return True
+    return False
+def teleport():
+    global player_x
+    global player_y
+    while True:
+        new_x = randint(1, 63)
+        new_y = randint(1, 47)
+        
+        if new_x != robot_x and new_y != robot_y:
+            player_x = new_x
+            player_y = new_y
+            break
+
+
+def rob():
+    global robot
+    global game
+    global player_x
+    global player_y 
+    global robot_x
+    global robot_y
+    robot = Circle((10 * robot_x + 5, 10 * robot_y + 5), 5, filled=True, color="Red")
+    if player_x > robot_x:
+        robot_x += 1
+    elif player_x < robot_x:
+        robot_x -= 1
+    if player_y > robot_y:
+        robot_y += 1
+    elif player_y < robot_y:
+        robot_y -= 1
+def py():
+    global player_x
+    global player_y 
+    global robot_x
+    global robot_y
+    global robot
+    
+    player = Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
+    key = update_when('key_pressed')
+    remove_from_screen(robot)
+    Text(key,(320, 100), size=12)
+    if key == 'KP_6':
+        player_x += 1
+    elif key == 'KP_4':
+        player_x -= 1
+    elif key == 'KP_8':
+        player_y += 1
+    elif key == 'KP_2':
+        player_y -= 1
+    elif key == 'KP_9':
+        player_x += 1 
+        player_y += 1
+    elif key == 'KP_7':
+        player_x -= 1 
+        player_y += 1 
+    elif key == 'KP_1':
+        player_x -= 1
+        player_y -= 1 
+    elif key == 'KP_3': 
+        player_x += 1 
+        player_y -= 1
+    if key == 't':
+        teleport()
+    if player_x == 63:
+        player_x = 1
+    if player_y == 47:
+        player_y = 1
+    if player_x == 0:
+        player_x = 63
+    if player_y == 0:
+        player_y = 47
+
+    remove_from_screen(player)
+    return
 def lines():
     line_x = 0
     line_y = 0
@@ -7,38 +92,43 @@ def lines():
     for x in range(0,640,10):
         Line((x,0),(x,640),thickness=.01,color='lightgray')
 
-def place_player(player_x,player_y):
-    Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
-begin_graphics()            
-lines()
+def start_screen():
+    key_text = Text("ROBOTS!!!", (320, 270), size=48)
+    key_text = Text("Press any key to start", (320, 100), size=12)
+    update_when('key_pressed')
+    clear_screen()
 
-player_x= 30
-player_y= 30 
+def GGame():
+    start_screen()
+    lines()
+    global game
+    global player_x
+    global player_y 
+    global robot_x
+    global robot_y
+    global robot
+    robot = Circle((1, 1), 1, filled=True)
+    game = True
+    player_x = randint(2,63)
+    player_y = randint(2,47)
+    robot_x = randint(2, 62)
+    robot_y = randint(2, 47)
+    while robot_x > player_x-10 and robot_x < player_x+10:
+        robot_x = randint(2, 47)
+    while robot_x > player_x-10 and robot_x < player_x+10:
+        robot_y = randint(2, 63)
+    while game:
+        if player_x == robot_x and player_y == robot_y:
+            game = False
+        py()
+        rob()
+
+    lost = loose()
+    if not lost:
+        GGame()
 
 
-while True: 
-    player = Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
-    key = update_when('key_pressed')
-    remove_from_screen(player)
-    if key == 'Right':
-        player_x += 1
-    if key == 'Left':
-        player_x -= 1
-    if key == 'Up':
-        player_y += 1
-    if key == 'Down': 
-        player_y -= 1
-    if player_x >= 63:
-        player_x= 63
-    if player_x <=0:
-        player_x=0
-    if player_y >=47:
-        player_y=47
-    if player_y <=0:
-        player_y = 0
+begin_graphics(title = "Robots")
 
-    if key == 'q':
-        break
-
-update_when('key_pressed')
-end_graphics() 
+GGame()
+end_graphics()
