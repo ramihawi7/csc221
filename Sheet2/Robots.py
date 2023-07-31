435613baef01c4a34b927d6ab48892941c35c567
Rami Hawi <ramihawi7@gmail.com>
2023-07-31 09:49:33 -0700

    aa

--- Sheet2/Robots.py
+++ Sheet2/Robots.py
@@ -1,16 +1,52 @@
 from gasp import *
 import time
 from random import randint
-def loose():
-    time.sleep(1)
-    clear_screen()
-    Text("You Lost :'c",(320, 270), size=40)
-    key_text = Text("Press esc to stop playing", (320, 100), size=12)
-    en = update_when('key_pressed')
-    clear_screen()
-    if en == "Escape":
-        return True
-    return False
+
+def robots_collioded():.
+    while True :
+        player_y != junk_y and player_x != junk_x
+
+def junkgen():
+    global junk_x
+    global junk_y
+    global player_x
+    global player_y 
+    global robot_x
+    global robot_y
+    global robot
+    for zz in range(len(robot_x)-1):
+        z = zz + 1
+        while z < len(robot_x)-1:    
+            if robot_x[zz] == robot_x[z] and robot_y[zz] == robot_y[z]:
+                print("robot lost")
+                junk_x.append(robot_x.pop(z))
+                junk_y.append(robot_y.pop(z))
+                robot.pop(z)
+                robot_x.pop(zz)
+                robot_y.pop(zz)
+                robot.pop(zz)
+            z += 1
+        for m in range(len(junk_x)-1):
+            if robot_x[zz] == junk_x[m] and robot_y[zz] == junk_x[m]:
+                robot_x.pop(zz)
+                robot_y.pop(zz)
+                robot.pop(zz)
+    for jk in range(len(junk_x)):
+        Box((10 * junk_x[jk],10 *  junk_y[jk]), 10,10,color = color.GREEN)
+def robotcraft ():
+    global player_x
+    global player_y 
+    global robot_x
+    global robot_y
+    global robot
+    x = randint(2, 62)
+    y = randint(2, 47)
+    while x > player_x-10 and x < player_x+10:
+        x = randint(2, 47)
+    while y > player_y-10 and y < player_y+10:
+        y = randint(2, 63)
+    robot_x.append(x)
+    robot_y.append(y)
 def teleport():
     global player_x
     global player_y
@@ -21,8 +57,18 @@ def teleport():
         if new_x != robot_x and new_y != robot_y:
             player_x = new_x
             player_y = new_y
-            break
- 
+            
+def loose():
+    time.sleep(1)
+    clear_screen()
+    Text("You Lost :'c",(320, 270), size=40)
+    key_text = Text("Press esc to stop playing", (320, 100), size=12)
+    en = update_when('key_pressed')
+    clear_screen()
+    if en == "Escape":
+        return True
+    return False
+    
 def rob(i):
     global robot
     global game
@@ -31,16 +77,9 @@ def rob(i):
     global robot_x
     global robot_y
     
-    if i % 2 == 0:    
-        x = randint(2, 62)
-        y = randint(2,47)
-        while x > player_x-10 and x < player_x+10:
-            x = randint(2, 47)
-        while y > player_y-10 and y < player_y+10:
-            y = randint(2, 63)
-        robot_x.append(x)
-        robot_y.append(y)
-        
+    if i % 4 == 0:
+        robotcraft()
+
     for ra in range(len(robot_x)-1): 
         robot.append(Circle((10 * robot_x[ra] + 5, 10 * robot_y[ra] + 5), 5, filled=True, color="Red"))
         if player_x > robot_x[ra]:
@@ -51,12 +90,6 @@ def rob(i):
             robot_y[ra] += 1
         elif player_y < robot_y[ra]:
             robot_y[ra] -= 1
-junk_x=[]
-junk_y=[]
-junk=[]
-
-junk = Box(10 * junk_x,10 *  junk_y, 10,10,color = color.GREEN)
-junk.append(junk)
 def py():
     global player_x
     global player_y 
@@ -68,7 +101,6 @@ def py():
     key = update_when('key_pressed')
     for ra in robot:
         remove_from_screen(ra)
-    Text(key,(320, 100), size=12)
     if key == 'KP_Right 'or key == 'd':
         player_x += 1
     elif key == 'KP_Left 'or key == 'a':
@@ -125,28 +157,28 @@ def GGame(i):
     global robot_x
     global robot_y
     global robot
+    global junk_x
+    global junk_y
     robot_x = []
     robot_y = []
     robot = []
+
+    junk_y = []
+    junk_x = []
     game = True
     player_x = randint(2,63)
     player_y = randint(2,47)
-    x = randint(2, 62)
-    y = randint(2, 47)
-    while x > player_x-10 and x < player_x+10:
-        x = randint(2, 47)
-    while y > player_y-10 and y < player_x+10:
-        y = randint(2, 63)
-    robot_x.append(x)
-    robot_y.append(y)
+    robotcraft()
     while game:
         i += 1
-        for mm in range(len(robot_x)):
-            if player_x == robot_x[mm] and player_y == robot_y[mm]:
-                game = False
+       # for mm in range(len(robot_x)):
+            #if (player_x == robot_x[mm]) and (player_y == robot_y[mm]):
+                #game = False
+        junkgen()
         py()
         rob(i)
-
+        robots_collioded()
+        
     lost = loose()
     if not lost:
         GGame(i)
